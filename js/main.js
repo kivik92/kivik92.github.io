d19305ef1dd9609538d42dbf55a3c9e71fdb2176
kivik92 <vpkpoltavadkp@gmail.com>
2019-02-09 21:27:10 +0200

    Work at home

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
+    autoplay: false,
     dots: true,
     loop: true,
     items: 1
